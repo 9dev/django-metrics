c0ccb9f884a7b80c59c73e7fc15cbcd0e8fe64bc
9dev <9devmail@gmail.com>
2016-02-25 20:46:45 +0100

    Added another value metric.

--- demo/main/metrics.py
+++ demo/main/metrics.py
@@ -1,10 +1,24 @@
+from datetime import datetime
+
 from metrics.metrics import ValueMetric
 
 from .models import Article
 
 
+class AvgArticlesMetric(ValueMetric):
+    name = 'articles/year'
+
+    def get_value(self):
+        articles = Article.objects.all().values('created')
+        beginning = articles.order_by('created')[0]['created']
+        td = datetime.utcnow() - beginning.replace(tzinfo=None)
+
+        avg = articles.count() / (td.total_seconds() / (365*24*60*60))
+        return round(avg)
+
+
 class TotalArticlesMetric(ValueMetric):
-    name = 'Total articles'
+    name = 'total articles'
 
     def get_value(self):
         return Article.objects.count()
--- fts/Metrics.py
+++ fts/Metrics.py
@@ -6,19 +6,18 @@ class TestMetrics(BaseTestCase):
     def setUp(self):
         super(TestMetrics, self).setUp()
 
+    def test_can_see_single_value_metrics(self):
         # Harriet logs in as an admin.
         self.login_as_admin()
 
-    def test_can_see_single_value_metric(self):
-        # Harriet hits metrics dashboard.
+        # She hits metrics dashboard.
         self.get(name='metrics')
 
         # She sees a metric for total number of articles.
-        self.assertIn('Total articles: 10', self.get_text())
+        self.assertIn('total articles: 10', self.get_text())
 
-    def test_can_see_multiple_single_value_metrics(self):
-        # articles total count, avg articles per day
-        self.fail()
+        # She sees a metric for average number of articles.
+        self.assertIn('articles/year:', self.get_text())
 
     def test_can_see_line_chart_metric(self):
         self.fail()
