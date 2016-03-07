fd2936e5d2130019211bb1fc66907b56f4412652
9dev <9devmail@gmail.com>
2016-03-07 11:17:04 +0100

    Added support for multiple line chart metrics.

--- demo/main/metrics.py
+++ demo/main/metrics.py
@@ -12,12 +12,11 @@ class AvgArticlesMetric(ValueMetric):
     name = 'articles/year'
 
     def get_value(self):
-        articles = Article.objects.all().values('created')
-        beginning = articles.order_by('created')[0]['created']
-        td = datetime.utcnow() - beginning.replace(tzinfo=None)
-
-        avg = articles.count() / (td.total_seconds() / (365*24*60*60))
-        return round(avg)
+        year = timedelta(days=365)
+        last_year = datetime.utcnow().replace(tzinfo=timezone.utc) - year
+        articles = Article.objects.all().values('created').filter(created__gte=last_year)
+        avg = articles.count() / year.total_seconds()
+        return avg
 
 
 class TotalArticlesMetric(ValueMetric):
@@ -41,6 +40,28 @@ class NewSignupsMetric(LineChartMetric):
         users = User.objects.all().filter(date_joined__gte=last7days)
         signups = users.extra({'day': "date(date_joined)"}).values('day').annotate(signups=Count('pk'))
         data = {row['day']: row['signups'] for row in signups}
+
+        for day, n in ((last7days + timedelta(days=n), n) for n in range(7)):
+            day = str(day.date())
+            values.append([n+1, data.get(day, 0)])
+
+        return values
+
+
+class NewArticlesMetric(LineChartMetric):
+    name = 'new articles'
+    xlabel = 'Day'
+    ylabel = 'New articles'
+
+    def get_values(self):
+        values = []
+
+        now = datetime.utcnow().replace(tzinfo=timezone.utc)
+        last7days = now - timedelta(days=7)
+
+        articles = Article.objects.all().filter(created__gte=last7days)
+        publications = articles.extra({'day': "date(created)"}).values('day').annotate(publications=Count('pk'))
+        data = {row['day']: row['publications'] for row in publications}
 
         for day, n in ((last7days + timedelta(days=n), n) for n in range(7)):
             day = str(day.date())
--- fts/Metrics.py
+++ fts/Metrics.py
@@ -3,6 +3,8 @@ from time import sleep
 
 from django.contrib.auth.models import User
 
+from main.models import Article
+
 from ._base import BaseTestCase
 
 
@@ -13,10 +15,14 @@ class TestMetrics(BaseTestCase):
 
         dt = datetime.utcnow().replace(tzinfo=timezone.utc)
         day = timedelta(days=1)
+
         User.objects.create(username='a', date_joined=dt)
         User.objects.create(username='b', date_joined=dt-day*2)
         User.objects.create(username='c', date_joined=dt-day*6)
         User.objects.create(username='d', date_joined=dt-day*6)
+
+        Article.objects.bulk_create([Article(), Article(), Article()])
+        Article.objects.all().update(created=dt-day*3)
 
     def test_can_see_single_value_metrics(self):
         # Harriet logs in as an admin.
@@ -26,7 +32,7 @@ class TestMetrics(BaseTestCase):
         self.get(name='metrics')
 
         # She sees a metric for total number of articles.
-        self.assertIn('total articles: 10', self.get_text())
+        self.assertIn('total articles: 3', self.get_text())
 
         # She sees a metric for average number of articles.
         self.assertIn('articles/year:', self.get_text())
@@ -37,7 +43,7 @@ class TestMetrics(BaseTestCase):
 
         # She hits metrics dashboard.
         self.get(name='metrics')
-        sleep(2)
+        sleep(5)
 
         # She sees a metric for new sign-ups.
         expected_rows = '[1,0],[2,2],[3,0],[4,0],[5,0],[6,1],[7,0]'
@@ -47,5 +53,7 @@ class TestMetrics(BaseTestCase):
         self.assertIn('Day', self.browser.page_source)
         self.assertIn('Sign-ups', self.browser.page_source)
 
-        # new signups, new articles, active users
-        self.fail()
+        # She sees a metric for new articles.
+        expected_rows = '[1,0],[2,0],[3,0],[4,0],[5,3],[6,0],[7,0]'
+        self.assertIn(expected_rows, self.browser.page_source)
+        self.assertIn('New articles', self.browser.page_source)
